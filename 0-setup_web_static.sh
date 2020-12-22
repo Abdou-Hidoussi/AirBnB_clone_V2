1adf3ba146847b68d6be37ce5736ea39f94618d3
fadikoubaa19 <fadi.koubaa@gmail.com>
2020-12-22 22:09:31 +0100

    web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
-# Task 0
+# Prepare my web servers
 sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
-sudo echo "<html>
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+</html>"| sudo tee /data/web_static/releases/test/index.html
 sudo rm -f -R /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-TEST_STRING="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
-sudo sed -i "53i $TEST_STRING" /etc/nginx/sites-available/default
+msg="location /hbnb_static/ {\nalias /data/web_static/current;\n}"
+sudo sed -i "40i $msg" /etc/nginx/sites-enabled/default
 sudo service nginx restart
