11978f9f0e6b5be0e455c1a0151f37b485a92c0a
abdou-hidoussi <hidoussiabdou5@gmail.com>
2021-01-20 16:15:20 +0000

    initial

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script
+"""
+from models import storage
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def exeption(e):
+    """ Task 8 """
+    storage.close()
+
+
+@app.route('/states_list')
+def statesList():
+    """ Task 8 """
+    state = storage.all("State")
+    return render_template('7-states_list.html', state=state)
+
+
+if __name__ == "__main__":
+    app.run()
